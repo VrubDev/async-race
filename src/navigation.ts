4347082ed1cbaf4726707c7ffd5b63036b0373d1
Victoria Vrublevskaya <vivrubl16@gmail.com>
2026-08-20 20:22:54 +0400

    feat: add view switching logic between garage and winners

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,10 +5,28 @@ export function renderNavigation(): HTMLElement {
   const garageButton: HTMLButtonElement = document.createElement("button");
   garageButton.className = "button nav-button";
   garageButton.textContent = "To garage";
+  garageButton.addEventListener("click", (): void => {
+    const garage = document.querySelector(".garage-view");
+    const winners = document.querySelector(".winners-view");
+
+    if (garage && winners) {
+      garage.classList.remove("hidden");
+      winners.classList.add("hidden");
+    }
+  });
 
   const winnersButton: HTMLButtonElement = document.createElement("button");
   winnersButton.className = "button nav-button";
   winnersButton.textContent = "To winners";
+  winnersButton.addEventListener("click", (): void => {
+    const garage = document.querySelector(".garage-view");
+    const winners = document.querySelector(".winners-view");
+
+    if (garage && winners) {
+      garage.classList.add("hidden");
+      winners.classList.remove("hidden");
+    }
+  });
 
   nav.append(garageButton, winnersButton);
   return nav;
